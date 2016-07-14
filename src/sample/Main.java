7c951016e003b33035769a0523bc413440f13b8e
halfshelldell <simmonslydell@gmai.com>
2016-07-14 11:05:32 -0400

    add fps

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,19 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Main extends Application {
 
@@ -19,6 +22,7 @@ public class Main extends Application {
     static final int ANT_COUNT = 100;
 
     static ArrayList<Ant> ants = new ArrayList<>();
+    static long lastTimeStamp = 0;
 
     static void  createAnts() {
        for (int i = 0; i < ANT_COUNT; i++) {
@@ -37,6 +41,29 @@ public class Main extends Application {
         }
     }
 
+    static Ant moveAnt(Ant ant) {
+        ant.x += Math.random() * 2 - 1;
+        ant.y += Math.random() * 2 - 1;
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return ant;
+    }
+
+    static void moveAnts() {
+       ants = ants.parallelStream()
+               .map(Main::moveAnt)
+               .collect(Collectors.toCollection(ArrayList<Ant>::new));
+    }
+
+    static int fps(long currentTimeStamp) {
+        double diff = currentTimeStamp - lastTimeStamp;
+        double diffSeconds = diff / 1000000000;
+        return (int) (0.1 / diffSeconds);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
@@ -46,9 +73,20 @@ public class Main extends Application {
 
         Canvas canvas = (Canvas) primaryStage.getScene().lookup("#canvas");
         GraphicsContext context = canvas.getGraphicsContext2D();
+        Label fpsLabel = (Label) primaryStage.getScene().lookup("#fps");
 
         createAnts();
-        drawAnts(context);
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                moveAnts();
+                drawAnts(context);
+                fpsLabel.setText(fps(now) + "");
+                lastTimeStamp = now;
+            }
+        };
+        timer.start();
     }
 
 
